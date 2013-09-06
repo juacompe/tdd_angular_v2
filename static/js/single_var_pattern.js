c4d95f59a507a4a305cd185f295c9e744fe237b8
juacompe <juacompe@gmail.com>
2013-09-06 18:17:27 +0700

    - add single var pattern

---
+++ static/js/single_var_pattern.js
@@ -0,0 +1,11 @@
+var hello_world = function() {
+    var name = "roofimon";
+
+    var reverse_name = function() {
+        var e = name;
+        console.log("hello " + e + "!");
+        var name = "juacompe";
+    };
+
+    reverse_name();
+};
