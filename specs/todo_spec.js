712f6ee430592cef474d2fe01941579cca4745f4
juacompe <juacompe@gmail.com>
2013-09-07 23:25:28 +0700

    todo application - can add new item - can list items with their status

--- specs/todo_spec.js
+++ specs/todo_spec.js
@@ -1,6 +1,49 @@
-describe('sanity check', function() {
-    it('1 + 2 should = 3', function() {
-        expect(1 + 2).toEqual(3);
-    });
+describe('TodoCtrl', function() {
+    var $scope;
+    beforeEach(inject(function($rootScope, $controller){
+        $scope = $rootScope.$new();
+        $controller('TodoCtrl', {
+            $scope: $scope,
+        });
+    }));
+
+    describe('initialization', function() {
+        it('should have newItem as empty string', function() {
+            expect($scope.newItem).toEqual("");
+        }); // newItem
+
+        it('should have 3 items todo', function() {
+            expect($scope.todoItems.length).toEqual(3);
+            describe('first item', function() {
+                var firstItem = $scope.todoItems[0];
+                expect(firstItem.title).toEqual('Learn Angular');
+                expect(firstItem.done).toEqual(true);
+            });
+
+            describe('second item', function() {
+                var secondItem = $scope.todoItems[1];
+                expect(secondItem.title).toEqual('Build Angular App');
+                expect(secondItem.done).toEqual(false);
+            });
+
+            describe('third item', function() {
+                var thirdItem = $scope.todoItems[2];
+                expect(thirdItem.title).toEqual('Master Angular');
+                expect(thirdItem.done).toEqual(false);
+            });
+        }); // 3 items todo
+    }); // initialization
+
+    describe('addNewItem', function() {
+        it('should add new item into todo list', function() {
+            $scope.newItem = 'Eat Lunch!!';
+            $scope.addNewItem();
+            expect($scope.newItem).toEqual(''); // clear textbox after added
+            expect($scope.todoItems.length).toEqual(4);
+            var newlyAddedItem = $scope.todoItems[3];
+            expect(newlyAddedItem.title).toEqual('Eat Lunch!!');
+            expect(newlyAddedItem.done).toEqual(false);
+        });
+    }); // addNewItem
 });
 
--- static/js/todo.js
+++ static/js/todo.js
@@ -1,1 +1,16 @@
-// Code goes here
+function TodoCtrl($scope) {
+    $scope.newItem = '';
+    $scope.todoItems = [
+        { title: 'Learn Angular', done: true },
+        { title: 'Build Angular App', done: false },
+        { title: 'Master Angular', done: false },
+    ];
+    
+    $scope.addNewItem = function() {
+        $scope.todoItems.push({
+            title: $scope.newItem,
+            done: false,
+        });
+        $scope.newItem = '';
+    }
+};
