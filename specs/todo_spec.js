a4b9cd0465c3a4af5a31f0df262ed9ce3c669e55
Chokchai <juacompe@roofimons-MacBook-Pro-2.local>
2013-09-20 15:19:08 +0700

    - change structure of item from string to object (name: <value>) - create TodoService - mock TodoService in Test - now can list items from Firebase

--- specs/todo_spec.js
+++ specs/todo_spec.js
@@ -6,11 +6,34 @@ describe('TodoController', function() {
         // $controller('name', configuration);
         $controller('TodoController', {
             $scope: $scope,
+            TodoService: null,
         });
     }));
 
     it('should have 2 items after initialized', function() {
         expect($scope.items.length).toEqual(2);
+    });
+
+    it('should have Learn Angular as first item', function() {
+        var firstItem = $scope.items[0];
+        expect(firstItem.name).toEqual('Learn Angular');
+    });
+
+    it('should have Build Angular as second item', function() {
+        var secondItem = $scope.items[1];
+        expect(secondItem.name).toEqual('Build Angular');
+    });
+
+    describe('add', function() {
+        it('should have 3 items after added', function() {
+            $scope.add('Eat lunch');
+            expect($scope.items.length).toEqual(3);
+        });
+        it('should clear new item box after added', function() {
+            $scope.newItem = 'Eat lunch';
+            $scope.add('Eat lunch');
+            expect($scope.newItem).toEqual('');
+        });
     });
 });
 
--- static/js/todo.js
+++ static/js/todo.js
@@ -1,6 +1,35 @@
-function TodoController($scope) {
+todoApp = angular.module('TodoApp', ['ngResource']);
+todoApp.factory('TodoService', TodoService);
+injectList = [
+    '$scope', // first arg to inject
+    'TodoService', // second arg to inject
+    TodoController, // last item -- the real controller class
+];
+todoApp.controller('TodoController', injectList);
+
+function TodoController($scope, TodoService) {
     $scope.items = [
-        'Learn Angular',
-        'Build Angular',
+        {name: 'Learn Angular'},
+        {name: 'Build Angular'},
     ]; 
-}
+
+    $scope.add = function(newItem) {
+        $scope.items.push(newItem);
+        $scope.newItem = '';
+    }
+
+    $scope.load = function() {
+        $scope.items = TodoService.query();
+    }
+};
+
+function TodoService($resource) {
+    var url, defaultParams, actions;
+    url = 'https://juacompe-todo-app.firebaseio.com/todos2.json';
+    defaultParams = {};
+    actions = {
+        save: { method: 'PUT', isArray: true }
+    };
+    return $resource(url, defaultParams, actions);
+};
+
